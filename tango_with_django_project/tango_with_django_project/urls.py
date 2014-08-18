17482ced8be02a43c15d98b37755b71944674b6b
Hakan Sencer Yıldız <hakansencer.yildiz@sahibinden.com>
2014-08-19 00:07:53 +0300

    drag and drop çalışıyorum ülen

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     url(r'', include('social_auth.urls')),
     url(r'^myrango/', include('myrango.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^$', include('myrango.urls')),
+    url(r'^$', include('myrango.urls')),
     url(r'^rango/',include('rangotest.urls')),
 )
 
